21af4e35f3831a7bef79dea9e278b6de037549c9
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-01-30 14:59:30 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Nikita Zhdanov on 2024-01-30.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
