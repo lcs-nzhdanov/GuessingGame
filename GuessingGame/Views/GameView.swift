e1ff8cf34cc4c745940ed22918434d419f2b889b
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-02-02 09:04:32 -0500

    Fixed error when user inputs string

--- GuessingGame/Views/GameView.swift
+++ GuessingGame/Views/GameView.swift
@@ -13,6 +13,7 @@ struct GameView: View {
     
     // What number the user has guessed
     @State var selectedNumber = 50
+    @State var givenInput = ""
     
     // What number the computer wants the user to guess
     @State var target = Int.random(in: 1...100)
@@ -34,13 +35,7 @@ struct GameView: View {
                 Text("Guess what it is!")
                     .font(.headline)
                 
-                Stepper(value: $selectedNumber, in: 1...100) {
-                    HStack {
-                        Text("Make a guess:")
-                        Spacer()
-                        Text("\(selectedNumber)")
-                    }
-                }
+                TextField("Make a guess", text: $givenInput)
                 
                 Button {
                     checkGuess()
@@ -93,13 +88,18 @@ struct GameView: View {
     // MARK: Functions
     func checkGuess() {
         
+        guard let selectedNumber = Int(givenInput) else {
+            feedback = "Please provide an integer"
+            return
+        }
+        
         // Provide feedback to the user
         // When should they guess higher?
         // When should then guess lower?
         // FILL IN THIS CODE
         if target < selectedNumber {
             feedback = "My number is lower!"
-        } else if target > selectedNumber{
+        } else if target > selectedNumber {
             feedback = "My number is higher!"
         }
         
@@ -113,7 +113,7 @@ struct GameView: View {
     func reset() {
 
         // Start the user back at 50
-        selectedNumber = 50
+        givenInput = ""
         
         // Have the computer guess a new number
         target = Int.random(in: 1...100)
